bbb16045e871839b84e68f24e0e92c72a940f54f
Rinku Sharma <rinkuvatscs@gmail.com>
2018-06-07 21:17:40 +0530

    swagger added

--- src/main/java/com/woocation/auth/client/SwaggerConfiguration.java
+++ src/main/java/com/woocation/auth/client/SwaggerConfiguration.java
@@ -25,6 +25,6 @@ public class SwaggerConfiguration {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.woocation.auth.dashboard")).paths(PathSelectors.any()).build();
+				.apis(RequestHandlerSelectors.basePackage("com.woocation.auth.client")).paths(PathSelectors.any()).build();
 	}
 }
